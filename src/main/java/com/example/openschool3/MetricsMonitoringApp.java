69aa9d0eccf9d3320439da4e7c7a697c21ab16cf
wbryon <tamirbudaev.habibula@gmail.com>
2024-05-02 16:15:52 +0300

    git init

---
+++ src/main/java/com/example/openschool3/MetricsMonitoringApp.java
@@ -0,0 +1,13 @@
+package com.example.openschool3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsMonitoringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsMonitoringSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/openschool3/MetricsMonitoringAppTests.java
@@ -0,0 +1,13 @@
+package com.example.openschool3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsMonitoringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
